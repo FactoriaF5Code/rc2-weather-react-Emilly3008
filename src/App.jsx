038de3ee54cf495b8c35167632c4d464b6169169
Emilly3008 <151835514+Emilly3008@users.noreply.github.com>
2024-01-19 15:19:50 +0100

    estoy intentando qu eme salga la alerta de error, que pongo todoslos campos son abligatorios

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,35 @@ import { LoadingButton } from '@mui/lab';
 import { Box, Container, TextField, Typography } from '@mui/material';
 import { useState } from 'react';
 
+const API_WEATHER = 'https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=';
 export default function App() {
 
   const [city, setCity] = useState('');
-  const onSubmit = e => {
+  const { loading, setLoading } = useState(false);
+  const [error, setError] = useState({
+  error:false,
+  message:'',
+
+});
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log("submit");
-  }
+    setLoading(true);
+    setError({
+      error:false,
+      message:'',
+    })
+    try {
+      if (!city.trim()) throw { message: 'El campo ciudad es obligatorio' };
+    } catch (error) {
+      console.log(error);
+      setError({
+        error:true,
+        message:error.message,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Container
@@ -38,17 +60,33 @@ export default function App() {
           fullWidth
           value={city}
           onChange={(e) => setCity(e.target.value)}
-      />
+          error={error.error}
+          helperText={error.message}
 
-      <LoadingButton
-        type='submit'
-        variant='contained'
-        loading={false}
-        loadingIndicator='Cargando...'
+        />
+
+        <LoadingButton
+          type='submit'
+          variant='contained'
+          loading={loading}
+          loadingIndicator='Cargando...'
+        >
+          Buscar
+        </LoadingButton>
+      </Box>
+
+      <Typography
+        textAling='center'
+        sx={{ marginTop: 2, fontsize: '10px' }}
       >
-        Buscar
-      </LoadingButton>
-    </Box>
+        Powered by:{''}
+        <a
+          href='https://www.weatherapi.com/'
+          title='Weather API'
+        >
+          WeatherAPI.com
+        </a>
+      </Typography>
     </Container >
   );
 }
