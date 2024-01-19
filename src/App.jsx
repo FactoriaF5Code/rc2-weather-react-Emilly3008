a5d52caa351a806de8da1f5d05cf5b2b32744ed4
Emilly3008 <151835514+Emilly3008@users.noreply.github.com>
2024-01-19 13:37:14 +0100

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+import { Box, Container, TextField, Typography, LoadingButtom } from '@mui/material';
+import { useState } from 'react';
+
 export default function App() {
-  return(
-<>
-<h1>Mi App del tiempo</h1>
-</>
+
+  const [city, setCity] = useState('');
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log("submit");
+  }
+
+  return (
+    <Container
+      maxWidht='xs'
+      sx={{ marginTop: 2 }}
+    >
+      <Typography
+        variant='h3'
+        component='h1'
+        aling='center'
+        gutterBottom
+      >
+        Weather App
+      </Typography>
+      <Box
+        sx={{ display: 'grid', gap: 2 }}
+        component='form'
+        autoComplete='off'
+        onSubmit={onSubmit}
+      >
+        <TextField
+          id='city'
+          label='ciudad'
+          variant='outlined'
+          size='small'
+          required
+          fullWidth
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+      />
+
+      <LoadingButtom
+        type='submit'
+        variant='contained'
+        loading={false}
+        loadingIndicator='Cargando...'
+      >
+      </LoadingButtom>
+    </Box>
+    </Container >
   );
 }
